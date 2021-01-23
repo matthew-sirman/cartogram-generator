9443dbd1567937f702a258ce3b5b9f6e9cfd2533
matthew-sirman <ms2649@cam.ac.uk>
2021-01-23 15:32:29 +0000

    Added image generation

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,6 @@
 module Main where
 
-import Graphics.Gloss
 import Helper.MatchGenerator
 
-window :: Display
-window = InWindow "Helper" (200, 200) (200, 200)
-
-background :: Color
-background = blue
-
 main :: IO ()
-main = do
-    p <- createHelperImage "countries.txt"
-    display window background p
-    
+main = matchMain
