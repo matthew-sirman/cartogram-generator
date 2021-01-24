f4e19d6d06392dc6bac5431f9f8e6abcaee6a900
LRudL <55555739+LRudL@users.noreply.github.com>
2021-01-24 14:09:37 +0200

    Fix scale factor calculation, etc.

--- src/Scale.hs
+++ src/Scale.hs
@@ -5,11 +5,32 @@ import Data.Time
 import Data.Set (fromList,toList)
 import qualified Data.Map as M
 
-getStatisticScales :: CountryData -> UTCTime -> (M.Map String Float)
-getStatisticScales database date =
-    M.fromList [(cc, (let Just v = M.lookup cc statistics in v) / totalStats) | cc <- availableCodes]
+{-
+getRawData database date = 
+    M.fromList [(fst t, (let Just x = (snd t) in x)) | t <- raw, (snd t) /= Nothing]
+    where raw = [(countryCode, fetchData database countryCode date) | countryCode <- M.keys (countryData database)]
+
+getTotal rawData =
+    (sum . map snd) $ M.assocs rawData
+-}
+
+getRelativeStats :: CountryData -> UTCTime -> (M.Map String Float)
+getRelativeStats database date =
+    M.fromList [(cc,(let Just v = M.lookup cc statistics in v) / totalStats) | cc <- availableCodes]
     
     where raw = [(countryCode, fetchData database countryCode date) | countryCode <- M.keys (countryData database)]
           statistics = M.fromList [(cc, (let Just x = v in x)) | (cc, v) <- raw, v /= Nothing]
           availableCodes = map fst (M.assocs statistics)
           totalStats = (sum . map snd) $ M.assocs statistics
+
+getStatisticScales countryAreas database date =
+    M.fromList
+        (map
+            (\(ccode, area) ->
+                (ccode, if ccode == "GBR" then 5 else 1))
+                    --totalArea * (M.findWithDefault 0 ccode relStats) / area))
+            countryAreaList)
+    where
+        countryAreaList = M.assocs countryAreas
+        totalArea = sum (map snd countryAreaList)
+        relStats = getRelativeStats database date
